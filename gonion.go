b96befbbfc6a4f64585d79de735949daedd6fb01
R4yan <yessou.rayan@gmail.com>
2021-09-19 15:08:30 +0200

    Update gonion.go

--- gonion.go
+++ gonion.go
@@ -1,65 +1,157 @@
 package gonion
 
-import(
-	"fmt"
+import (
 	"net/url"
 	"net/http"
 	"io/ioutil"
 	"encoding/json"
+  "log"
 )
 
 type Client struct {
-
     UserAgent string
     HttpClient *http.Client
 }
 
-func (c *Client) Summary() SSummary {
-	BaseURL, err := url.Parse("https://onionoo.torproject.org")
-    rel := &url.URL{Path: "/summary"}
+func (c *Client) Summary(args Params) SSummary {
+  req, err := c.SendRequest("/summary", args)
+
+  resp, err := c.HttpClient.Do(req)
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  defer resp.Body.Close()
+
+  if err != nil {
+    log.Fatal(err)
+  }
+  var Sum SSummary
+  body, err := ioutil.ReadAll(resp.Body)
+  json.Unmarshal([]byte(body), &Sum)
+  return Sum
+}
+
+func (c *Client) Details(args Params) SDetails {
+
+  req, err := c.SendRequest("/details", args)
+
+  resp, err := c.HttpClient.Do(req)
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  defer resp.Body.Close()
+
+  if err != nil {
+    log.Fatal(err)
+  }
+  var Det SDetails
+  body, err := ioutil.ReadAll(resp.Body)
+  json.Unmarshal([]byte(body), &Det)
+  return Det
+
+}
+
+func (c *Client) Bandwidth(args Params) SBandwidth {
+
+  req, err := c.SendRequest("/bandwidth", args)
+
+  resp, err := c.HttpClient.Do(req)
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  defer resp.Body.Close()
+
+  if err != nil {
+    log.Fatal(err)
+  }
+  var Ban SBandwidth
+  body, err := ioutil.ReadAll(resp.Body)
+  json.Unmarshal([]byte(body), &Ban)
+  return Ban
+
+}
+func (c *Client) Weights(args Params) SWeights {
+
+  req, err := c.SendRequest("/weights", args)
+
+  resp, err := c.HttpClient.Do(req)
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  defer resp.Body.Close()
+
+  if err != nil {
+    log.Fatal(err)
+  }
+  var Wei SWeights
+  body, err := ioutil.ReadAll(resp.Body)
+  json.Unmarshal([]byte(body), &Wei)
+  return Wei
+
+}
+func (c *Client) Clients(args Params) SClients {
+
+  req, err := c.SendRequest("/clients", args)
+
+  resp, err := c.HttpClient.Do(req)
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  defer resp.Body.Close()
+
+  if err != nil {
+    log.Fatal(err)
+  }
+  var Cli SClients
+  body, err := ioutil.ReadAll(resp.Body)
+  json.Unmarshal([]byte(body), &Cli)
+  return Cli
+
+}
+func (c *Client) Uptime(args Params) SUptime {
+
+  req, err := c.SendRequest("/bandwidth", args)
+
+  resp, err := c.HttpClient.Do(req)
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  defer resp.Body.Close()
+
+  if err != nil {
+    log.Fatal(err)
+  }
+  var Upt SUptime
+  body, err := ioutil.ReadAll(resp.Body)
+  json.Unmarshal([]byte(body), &Upt)
+  return Upt
+
+}
+
+func (c *Client)SendRequest(path string, args Params) (*http.Request, error){
+	  BaseURL, err := url.Parse("https://onionoo.torproject.org")
+    if err != nil {
+      log.Fatal(err)
+    }
+    rel := &url.URL{Path: path}
     u := BaseURL.ResolveReference(rel)
     req, err := http.NewRequest("GET", u.String(), nil)
     if err != nil {
-		panic(err)
+      log.Fatal(err)
     }
     req.Header.Set("Accept", "application/json")
     req.Header.Set("User-Agent", c.UserAgent)
-    resp, err := c.HttpClient.Do(req)
-    if err != nil {
-		panic(err)
+    q, err := args.QueryParams()
+    if err != nil{
+      log.Fatal(err)
     }
-    defer resp.Body.Close()
-	var Sum SSummary
-	body, err := ioutil.ReadAll(resp.Body)
-	json.Unmarshal([]byte(body), &Sum)
-    return Sum
-}
+    req.URL.RawQuery = q.Encode()
 
-func (c *Client) Details() SDetails {
-	BaseURL, err := url.Parse("https://onionoo.torproject.org")
-    rel := &url.URL{Path: "/details"}
-    u := BaseURL.ResolveReference(rel)
-    req, err := http.NewRequest("GET", u.String(), nil)
-    if err != nil {
-		panic(err)
-    }
-    req.Header.Set("Accept", "application/json")
-    req.Header.Set("User-Agent", c.UserAgent)
- 
-    resp, err := c.HttpClient.Do(req)
-    if err != nil {
-		panic(err)
-    }
-    defer resp.Body.Close()
-	var Sum SDetails
-	body, err := ioutil.ReadAll(resp.Body)
-	//fmt.Println(string(body))
-	json.Unmarshal([]byte(body), &Sum)
-	//fmt.Println(Sum.Relays)
-    return Sum
+    return req, nil
 }
-
-func Test() {
-    fmt.Println("testing aaaaaaaa")
-}
-
